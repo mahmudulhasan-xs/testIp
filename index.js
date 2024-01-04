e5d2416d0703bc4bec773975c95b610c285270d9
mahmudulhasan-xs <36738052+mahmudsagar@users.noreply.github.com>
2024-01-04 17:56:24 +0600

    Update trust proxy setting in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const ipapi = require('ipapi.co');
 const requestIp = require('request-ip');
 const app = express();
 
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 
 app.get('/', (req, res) => {
     const ip = requestIp.getClientIp(req);
