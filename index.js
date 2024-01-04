d4decb9dd45b1495ab2fc3269f3bf9f81dd64ef6
mahmudulhasan-xs <36738052+mahmudsagar@users.noreply.github.com>
2024-01-04 13:06:40 +0600

    Refactor code to improve performance and readability

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // initialize simple express app that will print ip address of the request on root route
 const express = require('express');
 const app = express();
-
+app.set('trust proxy', true);
 app.get('/', (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const ip = req.ip;
     res.send(`Your IP address is ${ip}`);
-    }
+}
 );
 
 app.listen(3300, () => console.log('Server is running on port 3300'));
